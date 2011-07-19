9b4b117771a3bca3e5f77b5c2ee1c689901efa57
Ilija Hadzic <ihadzic@research.bell-labs.com>
2011-07-19 14:39:33 -0400

    drm/radeon/kms: new parameter for vblank emulation
    the right way to do vblank emulation in push mode is to
    emit a fence after blit_copy request and then when the fence
    is signaled issue vblank emulation request; the problem is
    that there is no good mechanism to do real-time notification
    when the fence is signaled (they are all sort-of "garbage
    collected" when some other process requests to do something
    with, possibly different, fence). We need a fence-signaled
    interrupt, which may be doable using SCRATCH_INT interrupt
    (which is currently unused in radeon driver), but until we
    figure out how to put it into use, we have a less fortunate
    solution: as soon as copy request is issued, singal vblank
    and let the application proceed and submit new rendering
    commands. They will anyway happen after the copy due
    to FIFO nature of the ring, so we are safe when it comes
    to frame tearing. The only "fuzzy" part is that we just
    "trust" the GPU that copy will comnplete within the frame time.
    
    Signed-off-by: Ilija Hadzic <ihadzic@research.bell-labs.com>

--- drivers/gpu/drm/radeon/radeon_drv.c
+++ drivers/gpu/drm/radeon/radeon_drv.c
@@ -119,6 +119,7 @@ int radeon_disp_priority = 0;
 int radeon_hw_i2c = 0;
 int radeon_pcie_gen2 = 0;
 int radeon_num_virt_crtcs;
+int radeon_vbl_emu_async;
 
 MODULE_PARM_DESC(no_wb, "Disable AGP writeback for scratch registers");
 module_param_named(no_wb, radeon_no_wb, int, 0444);
@@ -167,6 +168,9 @@ module_param_named(pcie_gen2, radeon_pcie_gen2, int, 0444);
 
 MODULE_PARM_DESC(num_virt_crtcs, "Number of virtual crtcs to allocate");
 module_param_named(num_virt_crtcs, radeon_num_virt_crtcs, int, 0644);
+
+MODULE_PARM_DESC(vbl_emu_async, "Use asynchronous vblank emulation (0 = disable)");
+module_param_named(vbl_emu_async, radeon_vbl_emu_async, int, 0644);
 
 static int radeon_suspend(struct drm_device *dev, pm_message_t state)
 {
