8d02d13f1dde773327fb926ef7dd135b85bd8509
William Katsak <william.katsak@alcatel-lucent.com>
2011-11-29 10:46:15 -0500

    udlctd: include module header file in udlctd_usb.c
    Include linux/module.h in udlctd_usb.c.
    
    Resolves a compile-time warning.
    
    Signed-off-by: William Katsak <william.katsak@alcatel-lucent.com>

--- drivers/gpu/drm/ctd/udlctd/udlctd_usb.c
+++ drivers/gpu/drm/ctd/udlctd/udlctd_usb.c
@@ -17,6 +17,7 @@
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
+#include <linux/module.h>
 #include <linux/workqueue.h>
 #include <linux/delay.h>
 #include <linux/usb.h>
