ce52550948f5b4ea90da863e10e05eb8a679835d
Ilija Hadzic <ihadzic@research.bell-labs.com>
2012-03-26 13:36:10 -0400

    drm: allocate correct id_list size for a render node
    When a new render node is created, the number of elements
    of id_list allocated in drm_mode_group_init function should
    not be the sum of all CRTCs, encoders, and connectors that
    the device has, but the one specified by the ioctl that
    created the node.
    
    Signed-off-by: Ilija Hadzic <ihadzic@research.bell-labs.com>

--- drivers/gpu/drm/drm_stub.c
+++ drivers/gpu/drm/drm_stub.c
@@ -578,13 +578,12 @@ int drm_render_node_create_ioctl(struct drm_device *dev, void *data,
 	ret = drm_create_render_node(dev, &new_minor);
 	if (ret)
 		return ret;
-
-	ret = drm_mode_group_init(dev, &new_minor->mode_group);
+	total_ids = args->num_crtc + args->num_encoder + args->num_connector;
+	ret = drm_mode_group_init(&new_minor->mode_group, total_ids);
 	if (ret)
 		goto out_del;
 
 	ids_ptr = (uint32_t __user *)(unsigned long)args->id_list_ptr;
-	total_ids = args->num_crtc + args->num_encoder + args->num_connector;
 	for (i = 0; i < total_ids; i++) {
 		if (get_user(new_minor->mode_group.id_list[i], &ids_ptr[i])) {
 			ret = -EFAULT;
