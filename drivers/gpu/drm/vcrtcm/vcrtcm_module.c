86edc3201311b8ae5dfac676dfeb2d28ce3e68c2
William Katsak <william.katsak@alcatel-lucent.com>
2012-07-22 23:47:11 -0400

    vcrtcm: include vcrtcm_utils.h from system path
    Change #include for vcrtcm_utils.h to <vcrtcm/vcrtcm_utils.h>
    instead of simply "vcrtcm_utils.h"
    
    Signed-off-by: William Katsak <william.katsak@alcatel-lucent.com>

--- drivers/gpu/drm/vcrtcm/vcrtcm_module.c
+++ drivers/gpu/drm/vcrtcm/vcrtcm_module.c
@@ -21,7 +21,7 @@
 #include <linux/module.h>
 #include <linux/moduleparam.h>
 #include <vcrtcm/vcrtcm_sysfs.h>
-#include "vcrtcm_utils.h"
+#include <vcrtcm/vcrtcm_utils.h>
 #include "vcrtcm_private.h"
 
 struct list_head vcrtcm_pcon_list;
