2277d055bca7ba9d16e95e67612363d942baf80e
Martin Carroll <martin.carroll@alcatel-lucent.com>
2012-10-31 16:22:09 -0400

    vcrtcm: mutex the pconid_table
    This global data structure needs to be mutexed.
    
    Signed-off-by: Martin Carroll <martin.carroll@alcatel-lucent.com>

--- drivers/gpu/drm/vcrtcm/vcrtcm_structures.c
+++ drivers/gpu/drm/vcrtcm/vcrtcm_structures.c
@@ -106,6 +106,7 @@ struct vcrtcm_pcon_info *vcrtcm_alloc_pcon_info()
 {
 	int k;
 
+	mutex_lock(&pconid_table_mutex);
 	for (k = 0; k < MAX_NUM_PCONIDS; ++k) {
 		struct pconid_table_entry *entry = &pconid_table[k];
 		if (!entry->pcon_info) {
@@ -114,13 +115,16 @@ struct vcrtcm_pcon_info *vcrtcm_alloc_pcon_info()
 				GFP_KERNEL, &vcrtcm_kmalloc_track);
 			if (!pcon_info) {
 				VCRTCM_INFO("allocate of pcon_info failed\n");
+				mutex_unlock(&pconid_table_mutex);
 				return NULL;
 			}
 			pcon_info->pconid = k;
 			entry->pcon_info = pcon_info;
+			mutex_unlock(&pconid_table_mutex);
 			return pcon_info;
 		}
 	}
+	mutex_unlock(&pconid_table_mutex);
 	return NULL;
 }
 
@@ -130,30 +134,36 @@ void vcrtcm_dealloc_pcon_info(int pconid)
 
 	if (pconid < 0 || pconid >= MAX_NUM_PCONIDS)
 		return;
+	mutex_lock(&pconid_table_mutex);
 	entry = &pconid_table[pconid];
 	if (entry->pcon_info != NULL)
 		vcrtcm_kfree(entry->pcon_info, &vcrtcm_kmalloc_track);
 	entry->pcon_info = NULL;
+	mutex_unlock(&pconid_table_mutex);
 }
 
 struct vcrtcm_pcon_info *vcrtcm_get_pcon_info(int pconid)
 {
-	struct pconid_table_entry *entry;
+	struct vcrtcm_pcon_info *ret;
 
 	if (pconid < 0 || pconid >= MAX_NUM_PCONIDS)
 		return NULL;
-	entry = &pconid_table[pconid];
-	return entry->pcon_info;
+	mutex_lock(&pconid_table_mutex);
+	ret = pconid_table[pconid].pcon_info;
+	mutex_unlock(&pconid_table_mutex);
+	return ret;
 }
 
 int vcrtcm_pconid_valid(int pconid)
 {
-	struct pconid_table_entry *entry;
+	int ret;
 
 	if (pconid < 0 || pconid >= MAX_NUM_PCONIDS)
 		return 0;
-	entry = &pconid_table[pconid];
-	return entry->pcon_info != NULL;
+	mutex_lock(&pconid_table_mutex);
+	ret = pconid_table[pconid].pcon_info != NULL;
+	mutex_unlock(&pconid_table_mutex);
+	return ret;
 }
 
 /* Functions that are exported for PIMs to call */
