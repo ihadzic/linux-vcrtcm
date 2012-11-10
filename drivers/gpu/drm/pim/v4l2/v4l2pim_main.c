2be09d562b51dc417633b8d2ab1715c4edeea0cf
Martin Carroll <martin.carroll@alcatel-lucent.com>
2012-11-10 10:12:55 -0500

    v4l2pim: simplify logic for major-number allocation
    Signed-off-by: Martin Carroll <martin.carroll@alcatel-lucent.com>

--- drivers/gpu/drm/pim/v4l2/v4l2pim_main.c
+++ drivers/gpu/drm/pim/v4l2/v4l2pim_main.c
@@ -1053,19 +1053,17 @@ static int __init v4l2pim_init(void)
 	int r;
 
 	VCRTCM_INFO("v4l2 PCON, (C) Bell Labs, Alcatel-Lucent, Inc.\n");
+	r = alloc_chrdev_region(&dev, 0, V4L2PIM_MAX_MINORS, V4L2PIM_PIM_NAME);
+	if (r) {
+		VCRTCM_WARNING("can't get major device number\n");
+		return r;
+	}
+	v4l2pim_major = MAJOR(dev);
+	VCRTCM_INFO("using major %d\n", v4l2pim_major);
 	vcrtcm_pim_register(V4L2PIM_PIM_NAME, &v4l2pim_pim_funcs, &v4l2pim_pimid);
 	INIT_LIST_HEAD(&v4l2pim_minor_list);
 	vcrtcm_id_generator_init(&v4l2pim_minor_id_generator,
 					V4L2PIM_MAX_MINORS);
-	VCRTCM_INFO("Allocating/registering dynamic major number");
-	r = alloc_chrdev_region(&dev, 0, V4L2PIM_MAX_MINORS, "v4l2pim");
-	if (r) {
-		vcrtcm_pim_unregister(v4l2pim_pimid);
-		VCRTCM_ERROR("cannot get major device number\n");
-		return r;
-	}
-	v4l2pim_major = MAJOR(dev);
-	VCRTCM_INFO("Using major device number %d\n", v4l2pim_major);
 	if (V4L2PIM_VID_LIMIT_MAX < vid_limit) {
 		VCRTCM_WARNING("vid_limit (%d) too high, V4L2PIM_VID_LIMIT_MAX = %d\n", vid_limit, V4L2PIM_VID_LIMIT_MAX);
 		vid_limit = V4L2PIM_VID_LIMIT_MAX;
