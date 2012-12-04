da2b6ef414593b2f043e16999e114526cd78d313
Ilija Hadzic <ihadzic@research.bell-labs.com>
2012-12-04 14:22:50 -0500

    v4l2pim: do not keep stale pointer to mapping
    When buffer is unmapped, we need to set the mapping
    pointer in pcon structure to NULL. Otherwise,
    the realloc function may think that we have a mapping
    in place when we don't.
    
    Signed-off-by: Ilija Hadzic <ihadzic@research.bell-labs.com>

--- drivers/gpu/drm/pim/v4l2/v4l2pim_vcrtcm.c
+++ drivers/gpu/drm/pim/v4l2/v4l2pim_vcrtcm.c
@@ -34,10 +34,12 @@ static void v4l2pim_free_pb(struct v4l2pim_pcon *pcon, int flag)
 			pbd = pcon->pbd_fb[i];
 			pb_mapped_ram = pcon->pb_fb[i];
 			pcon->pbd_fb[i] = NULL;
+			pcon->pb_fb[i] = NULL;
 		} else {
 			pbd = pcon->pbd_cursor[i];
 			pb_mapped_ram = pcon->pb_cursor[i];
 			pcon->pbd_cursor[i] = NULL;
+			pcon->pb_cursor[i] = NULL;
 		}
 		if (pbd) {
 			BUG_ON(!pbd->gpu_private);
