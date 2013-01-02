a91f83d2e0d55e4b49eada2bae0b202a13474ee9
Ilija Hadzic <ihadzic@research.bell-labs.com>
2013-01-02 13:32:29 -0500

    drm/radeon/kms: stick to blit copy in vcrtcm push
    Starting with 3.8 kernel, the defauly copy function
    in radeon driver uses asynchronous DMA. However, we need
    to do some more testing with it before we decide to make
    it the mechanism of choice for VCRTCM push.
    
    This patch (temporarily) forces the use of known-working
    blit copy until we are fully satisfied with the performance
    and robustness of DMA copy.
    
    Signed-off-by: Ilija Hadzic <ihadzic@research.bell-labs.com>

--- drivers/gpu/drm/radeon/radeon_vcrtcm.c
+++ drivers/gpu/drm/radeon/radeon_vcrtcm.c
@@ -269,10 +269,12 @@ static int radeon_vcrtcm_push(int pconid, struct drm_crtc *scrtc,
 			if (!rdev->ring[ridx].ready)
 				DRM_ERROR("copy ring not ready (cursor)\n");
 			else if (!fence_c)
-				radeon_copy(rdev, saddr, daddr, num_pages, &fence_c);
+				radeon_copy_blit(rdev, saddr, daddr,
+						 num_pages, &fence_c);
 			else if (radeon_fence_signaled(fence_c)) {
 				radeon_fence_unref(&fence_c);
-				radeon_copy(rdev, saddr, daddr, num_pages, &fence_c);
+				radeon_copy_blit(rdev, saddr, daddr,
+						 num_pages, &fence_c);
 			} else
 				DRM_DEBUG("overlapped push (cursor)\n");
 			srcrtc->last_push_fence_c = fence_c;
@@ -304,10 +306,10 @@ static int radeon_vcrtcm_push(int pconid, struct drm_crtc *scrtc,
 	if (!rdev->ring[ridx].ready)
 		DRM_ERROR("copy ring not ready (framebuffer)\n");
 	else if (!fence_fb)
-		radeon_copy(rdev, saddr, daddr, num_pages, &fence_fb);
+		radeon_copy_blit(rdev, saddr, daddr, num_pages, &fence_fb);
 	else if (radeon_fence_signaled(fence_fb)) {
 		radeon_fence_unref(&fence_fb);
-		radeon_copy(rdev, saddr, daddr, num_pages, &fence_fb);
+		radeon_copy_blit(rdev, saddr, daddr, num_pages, &fence_fb);
 	} else
 		DRM_DEBUG("overlapped push (framebuffer)\n");
 	srcrtc->last_push_fence_fb = fence_fb;
